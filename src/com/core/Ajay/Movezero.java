2b6d2777850ae739372d83e34bb3028f8ddc52d7
Ajay Dinodiya <ajaydinodiya@gmail.com>
2026-03-20 13:01:21 +0530

    add Movezero class to implement moveZeroes method for shifting zeros in an array

---
+++ src/com/core/Ajay/Movezero.java
@@ -0,0 +1,17 @@
+package src.com.core.Ajay;
+
+    
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        
+    }    
+}
